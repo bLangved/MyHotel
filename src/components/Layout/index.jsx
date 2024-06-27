8c10c1c353ed9be1310a7c1594e3ff2936d31bcc
Bjørnar H. Langved <b.langved@icloud.com>
2024-06-27 13:58:12 +0200

    small cleanup

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
     <>
-      <Header />
       <main>
         <Outlet />
       </main>
-      <Footer />
     </>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,6 @@ import RoomList from "../../components/Rooms/RoomList";
 function Home() {
   return (
     <>
-      {/* <h1>Test</h1> */}
       <RoomList />
     </>
   );
